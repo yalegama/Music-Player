bd3efbc60d7ff51529fa43eea330feae22656986
Yalegama <nadunyalegama@gmai.com>
2021-09-25 12:20:28 +0530

    fourth commit

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
-/**Arrays */
+let songs=["Ai_kale_muluhadinma_oba_mata_adare","Iridata Palliyedi","Manike Mage Hithe"];
 
-let song=["Ai_kale_muluhadinma_oba_mata_adare","Iridata Palliyedi","Manike Mage Hithe"];
+/**handling Events */
 
-
-/**handlers */
-let title=document.getElementById('song_title');
-let title2=document.getElementById('song_title2');
-let playSong=document.getElementById('play');
-let progress=document.getElementById('progress');
 let cover=document.getElementById('cover');
+let title=document.getElementById('title');
+let progress=document.getElementById('progress');
+let shuffle=document.getElementById('shuffle');
+let preBtn=document.getElementById('preBtn');
+let playBtn=document.getElementById('playBtn');
+let nextBtn=document.getElementById('nextBtn');
+let repeat=document.getElementById('repeat');
 let audio=document.getElementById('audio');
-let play1=document.getElementById('play1');
-let preSong=document.getElementById('preSong');
-let nextSong=document.getElementById('nextSong');
 
-/**track of songs */
-let currentSong=0;
-/**loding songs */
-loadingSongs(song[currentSong]);
+/*current Song Index*/
+
+let currentSong=1;
+
+/**Function Loading */
+loadingSongs(songs[currentSong]);
 
 function loadingSongs(song){
+    cover.src=`images/${song}.jpeg`;
+    title.innerText=song;
+    audio.src=`songs/${song}.mp3`;
 
-    title.innerHTML=song;
-    title2.innerHTML=song;
-    cover.src=`./images/${song}.jpeg`;
-    audio.src=`./songs/${song}.mp3`;
-    
 }
 
-/**event listners */
-
-play1.addEventListener('click',()=>{
-    audio.play();
-});
-
+playBtn.addEventListener('click',()=>{
+    let isPlaying=
+})
 
