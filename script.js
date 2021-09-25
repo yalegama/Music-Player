247126e9ae9371b43de7fb1c017994ef4a4bcfc6
Yalegama <nadunyalegama@gmai.com>
2021-09-25 13:03:19 +0530

    sisth commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let songs=["Ai_kale_muluhadinma_oba_mata_adare","Iridata Palliyedi","Manike Mage
 
 /**handling Events */
 
+let container=document.getElementById('container');
 let cover=document.getElementById('cover');
 let title=document.getElementById('title');
 let progress=document.getElementById('progress');
@@ -14,7 +15,7 @@ let audio=document.getElementById('audio');
 
 /*current Song Index*/
 
-let currentSong=1;
+let currentSong=0;
 
 /**Function Loading */
 loadingSongs(songs[currentSong]);
@@ -27,6 +28,44 @@ function loadingSongs(song){
 }
 
 playBtn.addEventListener('click',()=>{
-    let isPlaying=
+    let isPlaying=container.classList.contains('play');
+    if(isPlaying){
+        pauseSong();
+    }else{
+        playSong();
+    }
 })
 
+/**pause function */
+function pauseSong(){
+    container.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    audio.pause();
+    
+}
+
+/**Play Song Function */
+function playSong(){
+    container.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+    audio.play();
+}
+
+
+/**Play Next Song  */
+nextBtn.addEventListener('click',()=>{
+    currentSong++;
+    loadingSongs(songs[currentSong]);
+    playSong();
+})
+
+/**Previous Song */
+
+preBtn.addEventListener('click',()=>{
+    currentSong--;
+
+    loadingSongs(songs[currentSong]);
+    playSong();
+})
